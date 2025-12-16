8c37318fc88d25114ae8922458d6bc95f93cddc1
ennabirha aymane <ennabirhaayman@gmail.com>
2025-12-16 18:17:04 +0100

    first commit

---
+++ tp30/src/main/java/com/example/tp30/Tp30Application.java
@@ -0,0 +1,13 @@
+package com.example.tp30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp30Application.class, args);
+	}
+
+}
---
+++ tp30/src/test/java/com/example/tp30/Tp30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
